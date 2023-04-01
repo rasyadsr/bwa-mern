a2fa880cf22ce3ad691fb67f9167f2be312ff4c6
Mochamad Rasyad <m.rasyad.cr7@gmail.com>
2023-04-01 16:49:15 +0700

    feat(client): create most picked & categories component

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Header from '../parts/Header'
 import Hero from '../parts/Hero'
 import jsonLandingPage from '../json/landingPage.json'
+import MostPicked from '../parts/MostPicked'
+import Categories from '../parts/Categories'
 
 export default function LandingPage() {
+  const refMostPicked = useRef(null)
+
   return (
     <>
       <Header />
-      <Hero data={jsonLandingPage.hero} />
+      <Hero refMostPicked={refMostPicked} data={jsonLandingPage.hero} />
+      <MostPicked
+        refMostPicked={refMostPicked}
+        data={jsonLandingPage.mostPicked}
+      />
+      <Categories data={jsonLandingPage.categories} />
     </>
   )
 }
